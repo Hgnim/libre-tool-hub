07d1853667aed445d78e29ff77494f71ed4a3876
Hgnim <hgn1m@outlook.com>
2025-12-23 18:41:14 +0800

    完善了国际化功能

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import {useTitle} from "@vueuse/core";
 import {autoUseI18n} from "@/utils/i18nUtils.ts";
+import {autoLoadLocale} from "@/ts/global/vue/autoLoadLocale.ts";
 
 const {gt}=autoUseI18n();
+const lp:string="view_Home";
 useTitle(gt('global.title'));
+
+autoLoadLocale(lp);//使用一体化自动加载语言
 </script>
 
 <template>
-  <h1>主页</h1>
+  <h1>{{gt(`${lp}.tmp`)}}</h1>
   <p>{{gt('global.title')}}</p>
 </template>
 
