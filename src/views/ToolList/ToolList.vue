36c16341740a959a0253dec41b30786ecbb66ce8
Hgnim <hgn1m@outlook.com>
2026-02-19 19:04:49 +0800

    工具列表中的工具跳转元素更改为卡片

--- src/views/ToolList/ToolList.vue
+++ src/views/ToolList/ToolList.vue
@@ -1,38 +1,86 @@
 <script setup lang="ts">
+import { autoLoadLocale } from "@/ts/global/vue/autoLoadLocale";
 import {autoUseI18n} from "@/utils/i18nUtils.ts";
 import {useTitle} from "@vueuse/core";
 
-const {lt:t,gt}=autoUseI18n();
-useTitle(`${t('title')}${gt('global.title')}`);
+const {gt:t}=autoUseI18n();
+const lp:string="view_ToolList";
+
+autoLoadLocale(lp, () => {
+  useTitle(`${t(`${lp}.title`)}${t('global.title')}`);
+});
 </script>
 
 <template>
-<router-link :to="{name: 'tool_baseConversion'}">{{t('baseConv')}}</router-link>
-  <br>
-  <router-link :to="{name: 'tool_stringAndBaseConversion'}">{{t('stringAndBaseConv')}}</router-link>
-  <br>
-  <router-link :to="{name: 'tool_qrcode'}">{{t('qrcode')}}</router-link>
-  <br>
-  <router-link :to="{name: 'tool_markdown-pane'}">markdown pane工具</router-link>
+  <div class="container pt-2">
+    <div class="row">
+      <router-link :to="{name: 'tool_baseConversion'}"
+                   class="col-3 router-link_toollist"
+      >
+        <div class="card toollist">
+          <div class="card-header">
+            {{t(`${lp}.baseConv.title`)}}
+          </div>
+          <div class="card-body">
+            {{t(`${lp}.baseConv.description`)}}
+          </div>
+        </div>
+      </router-link>
+      <router-link :to="{name: 'tool_stringAndBaseConversion'}"
+                   class="col-3 router-link_toollist"
+      >
+        <div class="card toollist">
+          <div class="card-header">
+            {{t(`${lp}.stringAndBaseConv.title`)}}
+          </div>
+          <div class="card-body">
+            {{t(`${lp}.stringAndBaseConv.description`)}}
+          </div>
+        </div>
+      </router-link>
+      <router-link :to="{name: 'tool_qrcode'}"
+                   class="col-3 router-link_toollist"
+      >
+        <div class="card toollist">
+          <div class="card-header">
+            {{t(`${lp}.qrcode.title`)}}
+          </div>
+          <div class="card-body">
+            {{t(`${lp}.qrcode.description`)}}
+          </div>
+        </div>
+      </router-link>
+      <router-link :to="{name: 'tool_markdown-pane'}"
+                   class="col-3 router-link_toollist"
+      >
+        <div class="card toollist">
+          <div class="card-header">
+            {{t(`${lp}.markdown-pane.title`)}}
+          </div>
+          <div class="card-body">
+            {{t(`${lp}.markdown-pane.description`)}}
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
-</style>
-
-<i18n>
-{
-  "zh-CN": {
-    "title": "工具列表-",
-    "baseConv": "进制转换",
-    "stringAndBaseConv": "文本转进制",
-    "qrcode": "二维码工具"
-  },
-  "en-US": {
-    "title": "Tool list - ",
-    "baseConv": "Base conversion",
-    "stringAndBaseConv": "Text to n-base",
-    "qrcode": "Qrcode tool"
+.card{
+  &.toollist{
+    height: 10rem;
+    .card-body{
+      overflow-y: scroll;
+    }
   }
 }
-</i18n>
+.router-link_toollist{
+  text-decoration: none;
+  /*
+  *{
+      text-decoration: none;
+  }
+  */
+}
+</style>
