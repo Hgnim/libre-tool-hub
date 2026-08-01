cf2640395d3c04caf0e1078be7b8af9f891e94a5
Hgnim <hgn1m@outlook.com>
2026-08-01 20:34:34 +0800

    颜色选取器完善，新增不同类型的颜色数据输入并互相转换的功能

--- src/views/tools/colorPicker/colorPicker.vue
+++ src/views/tools/colorPicker/colorPicker.vue
@@ -2,42 +2,70 @@
 import {ChromePicker,RGBSliders,HSVSliders,HSLSliders,tinycolor} from "vue-color";
 import {type Ref, ref} from "vue";
 import {getRgbaString} from "@/views/tools/colorPicker/ts/util/vue-color.ts";
+import {autoUseI18n} from "@/utils/i18nUtils.ts";
+import {useTitle} from "@vueuse/core";
 
-const colorPickerModel:Ref<string> = ref('#000');
+const {lt:t,gt}=autoUseI18n();
+useTitle(`${t('title')}${gt('global.title')}`);
+
+//const hexBox:Ref<HTMLInputElement|null>=ref(null);
+//const rgbBox:Ref<HTMLInputElement|null>=ref(null);
+//const rgbaBox:Ref<HTMLInputElement|null>=ref(null);
+//const prgbBox:Ref<HTMLInputElement|null>=ref(null);
+//const hsvBox:Ref<HTMLInputElement|null>=ref(null);
+//const hslBox:Ref<HTMLInputElement|null>=ref(null);
+
+type ColorPickerModelType={
+  value:string,
+}
+class ColorPickerModel{
+  private _value:string='';
+  get value():string{
+    return this._value;
+  }
+  set value(v:string){
+    this._value=v;
+  }
+}
+const colorPickerModel:Ref<ColorPickerModelType>=ref(new ColorPickerModel());
+
+function box_onChange(et:EventTarget|null){
+  colorPickerModel.value.value=tinycolor((et as HTMLInputElement).value).toHexString();
+}
 </script>
 
 <template>
   <div id="color-picker" class="container">
-    <div class="row">
+    <div class="row unSelectable">
       <div class="col-12 col-md-3 col-lg-2 p-3">
-        <div class="color-show" :style="{'background-color':tinycolor(colorPickerModel).toHexString()}"></div>
+        <div class="color-show" :style="{'background-color':tinycolor(colorPickerModel.value).toHexString()}"></div>
       </div>
       <div class="col-12 col-md-6 col-lg-3">
         <div class="d-flex justify-content-center align-items-center w-100 h-100">
-          <div class="text-center">
-            <p>{{tinycolor(colorPickerModel).toHexString()}}</p>
-            <p>{{tinycolor(colorPickerModel).toRgbString()}}</p>
-            <p>{{getRgbaString(tinycolor(colorPickerModel))}}</p>
-            <p>{{tinycolor(colorPickerModel).toPercentageRgbString()}}</p>
-            <p>{{tinycolor(colorPickerModel).toHsvString()}}</p>
-            <p>{{tinycolor(colorPickerModel).toHslString()}}</p>
+          <div>
+            <input :value="tinycolor(colorPickerModel.value).toHexString()" type="text" @change="box_onChange($event.target)" class="text-center"/><!--ref="hexBox" -->
+            <input :value="tinycolor(colorPickerModel.value).toRgbString()" type="text" @change="box_onChange($event.target)" class="text-center mt-1"/><!--ref="rgbBox" -->
+            <input  :value="getRgbaString(tinycolor(colorPickerModel.value))" type="text" @change="box_onChange($event.target)" class="text-center mt-1"/><!--ref="rgbaBox"-->
+            <input  :value="tinycolor(colorPickerModel.value).toPercentageRgbString()" type="text" @change="box_onChange($event.target)" class="text-center mt-1"/><!--ref="prgbBox"-->
+            <input :value="tinycolor(colorPickerModel.value).toHsvString()" type="text" @change="box_onChange($event.target)" class="text-center mt-1"/><!--ref="hsvBox" -->
+            <input :value="tinycolor(colorPickerModel.value).toHslString()" type="text" @change="box_onChange($event.target)" class="text-center mt-1"/><!--ref="hslBox" -->
           </div>
         </div>
       </div>
       <div class="col-12 col-md-3 col-lg-2 p-3">
-        <div class="color-show" :style="{'background-color':tinycolor(colorPickerModel).toHexString()}"></div>
+        <div class="color-show" :style="{'background-color':tinycolor(colorPickerModel.value).toHexString()}"></div>
       </div>
       <div class="col-12 col-lg-5">
-        <ChromePicker v-model="colorPickerModel"/>
+        <ChromePicker v-model="colorPickerModel.value"/>
       </div>
       <div class="col-12 col-lg-4">
-        <RGBSliders v-model="colorPickerModel"/>
+        <RGBSliders v-model="colorPickerModel.value"/>
       </div>
       <div class="col-12 col-lg-4">
-        <HSVSliders v-model="colorPickerModel"/>
+        <HSVSliders v-model="colorPickerModel.value"/>
       </div>
       <div class="col-12 col-lg-4">
-        <HSLSliders v-model="colorPickerModel"/>
+        <HSLSliders v-model="colorPickerModel.value"/>
       </div>
     </div>
   </div>
@@ -56,3 +84,15 @@ const colorPickerModel:Ref<string> = ref('#000');
 </style>
 <style lang="scss" src="@/assets/scss/vue-color/vcolor-custom.scss"></style><!--需要全局引用。经检查，基本不会全局污染，并且在路由中使用“component: () => import('@/views/tools/colorPicker/colorPicker.vue'),”以按需加载该css-->
 <style scoped lang="scss" src="./scss/vue-color.scss"></style>
+<style scoped lang="css" src="@/assets/css/global/unSelect.css"></style>
+
+<i18n>
+{
+  "zh-CN": {
+    "title": "颜色选取器 - "
+  },
+  "en-US": {
+    "title": "Color Picker - "
+  }
+}
+</i18n>
